41e6d6af2802466ee276dfb38c18ef08499c8f7e
TheChief8128 <76656329+DragonRider8128@users.noreply.github.com>
2025-05-19 17:44:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    #Use on home network
-    #app.run(host="192.168.0.49",debug=True)
-
-    #localhost
-    app.run(debug=True)
+    app.run()
